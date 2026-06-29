0f03e03f994bb0f94a685f9111d8fb233626f579
RuiFernandes <Roy_f_5@hotmail.com>
2026-06-29 14:41:44 +0100

    Component installation backup

---
+++ SuperShop/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperShop.Data.Entitis;
+
+namespace SuperShop.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
