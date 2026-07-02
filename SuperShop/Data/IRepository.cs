17b76e4a23a31076a437327afe7f2d4107f9352e
RuiFernandes <Roy_f_5@hotmail.com>
2026-07-02 16:38:27 +0100

    Repository

---
+++ SuperShop/Data/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SuperShop.Data.Entitis;
+
+namespace SuperShop.Data
+{
+    public interface IRepository
+    {
+        void AddProduct(Product product);
+
+        Product GetProduct(int id);
+
+        IEnumerable<Product> GetProducts();
+
+        bool ProductExists(int id);
+
+        void RemoveProdutc(Product product);
+
+        Task<bool> SaveAllAsync();
+
+        void UpdateProducts(Product product);
+    }
+}
